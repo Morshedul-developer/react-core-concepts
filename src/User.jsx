2ff8fab6821f0ce0e88255a39ec7a2ffda382eff
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-18 19:30:58 +0600

    fixed a bug

--- src/User.jsx
+++ src/User.jsx
@@ -1,6 +1,7 @@
 const User = ({user}) => {
     return (
         <div className="card">
+            <h3>Id: {user.id}</h3>
             <h4>Name: {user.name}</h4>
             <p>Email: {user.email}</p>
         </div>
